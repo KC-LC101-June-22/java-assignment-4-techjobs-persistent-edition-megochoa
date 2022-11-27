d30a7bdbc11eab7d357a6888ad4067ba6628fe04
megochoa <102236786+megochoa@users.noreply.github.com>
2022-11-27 13:22:44 -0600

    Finished Pt 1 and 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location cannot be blank")
+    @Size(max = 75, message = "Location cannot exceed 75 characters")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
